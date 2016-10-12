b47d460c6ca64a3e47bdd4476793d4ad2a3e1e58
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-12 18:13:14 +0900

    Add files via upload

---
+++ IEEE-Conference Code.py
@@ -0,0 +1,13 @@
+﻿activate py27
+
+scrapy shell "https://www.ieee.org/conferences_events/index.html"
+
+a=response.xpath('//*[@id="inner-container"]/div[5]/div[2]/div[2]/div[2]/div[1]/div/div[1]/p/a/text()').extract()
+a
+
+b=response.xpath('//*[@id="inner-container"]/div[5]/div[2]/div[2]/div[2]/div[1]/div/div[1]/p/text()').extract()
+b
+
+for k in range(0,4):
+	print a[k].strip(),
+	print b[k].strip()
