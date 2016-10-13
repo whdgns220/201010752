b2e7822eae2d87c3424b22a6be5efa428fd02114
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-13 17:30:29 +0900

    Add files via upload

---
+++ KOSPI 200 Code.py
@@ -0,0 +1,42 @@
+﻿activate py27
+
+scrapy shell "https://www.google.com/finance/historical?q=KRX:KOSPI200&ei=Ujr_V8mvEsLY0gSWlIjACA"
+
+a = response.xpath('//*[@id="prices"]/table/tr/td[1]/text()').extract()
+
+a
+
+b = response.xpath('//*[@id="prices"]/table/tr/td[2]/text()').extract()
+
+b
+
+c = response.xpath('//*[@id="prices"]/table/tr/td[3]/text()').extract()
+
+c
+
+d = response.xpath('//*[@id="prices"]/table/tr/td[4]/text()').extract()
+
+d
+
+e = response.xpath('//*[@id="prices"]/table/tr/td[5]/text()').extract()
+
+e
+
+f = response.xpath('//*[@id="prices"]/table/tr/td[6]/text()').extract()
+
+f
+
+for k in range(0,30):
+	print a[k].strip(),
+	print "-",
+	print b[k].strip(),
+	print "-",
+	print c[k].strip(),
+	print "-",
+	print d[k].strip(),
+	print "-",	
+	print e[k].strip(),
+	print "-",
+	print f[k].strip()
+	
+
