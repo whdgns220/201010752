74c46436c1ef6204c4e8a09d4d99628ecd8af95c
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-13 17:30:11 +0900

    Delete KOSPI 200 Code.py

--- KOSPI 200 Code.py
+++
@@ -1,8 +0,0 @@
-﻿activate py27
-
-scrapy shell "https://www.google.com/finance/historical?q=KRX%3AKOSPI200&ei=Igf_V4HVN5CF0gSP4K-IBw"
-
-a = response.xpath('//*[@id="prices"]/table/tr/td/text()').extract()
-
-a
-
