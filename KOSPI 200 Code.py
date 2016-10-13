da9ca759636f0ddc6c4b5d6ff66cc122b48bb603
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-13 13:38:01 +0900

    Add files via upload

---
+++ KOSPI 200 Code.py
@@ -0,0 +1,8 @@
+﻿activate py27
+
+scrapy shell "https://www.google.com/finance/historical?q=KRX%3AKOSPI200&ei=Igf_V4HVN5CF0gSP4K-IBw"
+
+a = response.xpath('//*[@id="prices"]/table/tr/td/text()').extract()
+
+a
+
