f1c4f8b302865fe117f6fdcda64c518a31ebd98c
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-17 15:51:30 +0900

    Add files via upload

---
+++ webdata-2.py
@@ -0,0 +1,35 @@
+﻿웹데이터-2 : 웹파일을 가져와서 자료구조에 넣기
+
+import urllib2
+url='http://archive.ics.uci.edu/ml/machine-learning-databases/horse-colic/horse-colic.data'
+res=urllib2.urlopen(url)
+html = res.read()
+res.close()
+print len(html)
+
+lines=html.splitlines()
+data=[]
+for line in lines:
+    data.append(line.split())
+print len(data), len(data[0])
+print data[0]
+
+for i in range(0,len(data)):
+    if len(data[i]) != len(data[0]):
+        print "wrong size at", i
+
+for i in range(0,20):
+    print data[i][3]
+
+sum=0
+cnt=0
+for i in range(0,20):
+    val=data[i][3]
+    if val is '?':
+        print i,"None"
+    else:
+        sum+=float(val)
+        cnt+=1
+        print i,val, sum
+average=float(sum/cnt)
+print "count={0} sum={1} average={2:2.2f}".format(cnt,sum,average)
