9cc685e6c612fded9b7dcc2c9da71059811be0fb
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-17 15:52:05 +0900

    Add files via upload

---
+++ REST_한글.py
@@ -0,0 +1,55 @@
+﻿##한글
+
+import locale
+locale.getdefaultlocale()
+
+import sys
+print sys.getdefaultencoding()
+
+print sys.stdin.encoding
+
+print sys.stdout.encoding
+
+
+str('한')
+
+u'한'
+
+print '한'
+
+print u'한'
+
+
+## 인터넷 한글 인코딩
+
+import urllib
+
+urllib.quote('/~jslm')
+
+urllib.quote('%')
+
+
+urllib.quote('#')
+
+
+urllib.urlencode({'key':'%3D'})
+
+
+##json(JavaScript Object Notation)
+
+
+
+import json
+In [31]:
+input = '''[
+    {"id":"001", "x":"2", "name":"Chuck"},
+    {"id":"002", "x":"3", "name":"jsl"}
+]'''
+
+type(input)
+
+
+Info=json.loads(input)
+
+info0['id']
+
