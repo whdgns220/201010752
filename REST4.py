d415f89ac382ef4f2d160c8d869672d1b80f051a
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-27 09:38:18 +0900

    Add files via upload

---
+++ REST4.py
@@ -0,0 +1,35 @@
+﻿import os
+KEY="4d43494948776864313137446c764b6f"
+TYPE='json'
+SERVICE='ListLocaldata470401S'
+START_INDEX='1'
+END_INDEX='10'
+
+
+url='http://openapi.seoul.go.kr:8088/'
+url+=KEY
+url+='/'
+url+=TYPE
+url+='/'
+url+=SERVICE
+url+='/'
+url+=START_INDEX
+url+='/'
+url+=END_INDEX
+url+='/'
+
+import requests
+myurl='http://openapi.seoul.go.kr:8088/4d43494948776864313137446c764b6f/xml/ListLocaldata470401S/1/319/'
+data=requests.get(myurl).text
+
+print data
+
+import lxml
+import lxml.etree
+import StringIO
+
+tree=lxml.etree.fromstring(data.encode('utf-8'))
+
+nodes=tree.xpath('//STATMAN')
+for node in nodes:
+    print node.text
