a01e631f605b72f56f38b05b54619f30f43cbad4
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-27 10:17:06 +0900

    Add files via upload

---
+++ REST2.py
@@ -0,0 +1,13 @@
+﻿def getKey(keyPath):
+    d=dict()
+    f=open(keyPath,'r')
+    for line in f.readlines():
+        row=line.split('=')
+        row0=row[0]
+        d[row0]=row[1].strip()
+    return d
+
+import os
+
+keyPath=os.path.join(os.getcwd(), 'src', 'key.properties')
+key=getKey(keyPath)
