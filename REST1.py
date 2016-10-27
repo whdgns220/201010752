3f6bc5977b19a68925961ad7a68a320f2feccdd9
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-10-27 10:16:15 +0900

    Add files via upload

---
+++ REST1.py
@@ -0,0 +1,44 @@
+﻿import requests
+url='http://freegeoip.net/json/'
+geostr=requests.get(url).text
+print geostr
+
+type(geostr)
+
+import json
+geojson=json.loads(geostr)
+
+type(geojson)
+
+print geojson['ip']
+
+geojson.get('ip')
+
+country=geojson.get('country_code')
+
+print country.decode('utf-8')
+
+import json
+import urllib
+
+def getCountry(ipAddress):
+    response = urllib.urlopen("http://freegeoip.net/json/"+ipAddress).read().decode('utf-8')
+    responseJson = json.loads(response)
+    return responseJson.get("country_code")
+
+print(getCountry("39.118.87.152"))
+
+import requests
+send_url = 'http://freegeoip.net/json/39.118.87.152'
+r = requests.get(send_url)
+
+j=json.loads(r.text)
+
+type(j)
+
+print j.keys()
+
+print j['city']
+
+for k,v in j.iteritems():
+    print k,"\t: ",v
