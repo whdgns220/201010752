18f64bece44637404db548f5a22925e225908f49
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-11-04 14:10:18 +0900

    Add files via upload

---
+++ webdata-6.py
@@ -0,0 +1,34 @@
+﻿import urllib2
+import requests
+urlperson='http://www.kbreport.com/player/list?key=이대호'
+urlbase="http://www.kbreport.com/leader/main?"
+url1="rows=20&order=oWAR&orderType=DESC&"
+url2="teamId=1&defense_no=2&year_from=2015&year_to=2015&split01=&split02_1=&split02_2=&r_tpa_count=&tpa_count=0"
+urlbaseball=urlbase+url1+url2
+print urlbaseball
+data=requests.get(urlbaseball).text
+print data[6000:7000]
+
+
+print data.find('top-score-top')
+print data.find('top-score end')
+
+
+mydata=data[6340:8353+len('top-score end')]
+import re
+p=re.compile(u'.승.+')
+
+
+found=p.findall(mydata)
+print found
+for item in found:
+    print item
+
+import requests
+urlkorbase='http://www.koreabaseball.com/Record/Player/HitterBasic/Basic1.aspx'
+data=requests.get(urlkorbase).text
+
+
+kosis='http://kosis.kr/statisticsList/statisticsList_01List.jsp?vwcd=MT_ZTITLE&parentId=A#SubCont'
+data=requests.get(urlkorbase).text
+print len(data)
