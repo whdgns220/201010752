9c996f26a3da572b1e42c4defdc25d4a6902a7ba
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-11-04 14:21:33 +0900

    Add files via upload

---
+++ webdata-8.py
@@ -0,0 +1,27 @@
+﻿%%writefile src/ds_web_data_paging.py
+import scrapy
+
+class QuoteItem(scrapy.Item):
+    text = scrapy.Field()
+    author = scrapy.Field()
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+    def parse(self, response):
+        for quote in response.xpath('//div[@class="quote"]'):
+            item = QuoteItem()
+            item['text'] = quote.xpath('span[@class="text"]/text()').extract_first()
+            item['author'] = quote.xpath('span/small/text()').extract_first()
+            print "crawling ",item['author']
+            yield item
+        next_page = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        if next_page:
+            next_page = response.urljoin(next_page)
+            print "--> visiting ",next_page
+            yield scrapy.Request(next_page, callback=self.parse)
+
+
+!scrapy runspider src/ds_web_data_paging.py -o src/ds_web_data_paging.json -t json --logfile src/ds_web_data_paging.logfile
