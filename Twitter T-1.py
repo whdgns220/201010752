51bd0c8b9e6a93d584e3e698736a6b09d22fc027
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-11-14 13:35:15 +0900

    Add files via upload

---
+++ Twitter T-1.py
@@ -0,0 +1,25 @@
+﻿import json
+import oauth2 as oauth
+
+Consumer_Key = "dc0eZ3zFaOFFdkUoWmIVms72W"
+Consumer_Secret = "qOQMweqA3GT0XqAyK9bWVTXS9TZJfHj78fBRvBVBzxqJRFmrNy"
+Access_Token = "786057611166773249-WYglj3fxUPW23sOfBRJboVAiIF5gaAt"
+Access_Token_Secret = "AZNzz8C2edrA85L0Mvg6WLKC0RUO6onKVyiVkcNe7BKti"
+
+consumer = oauth.Consumer(key=Consumer_Key, secret=Consumer_Secret)
+access_token = oauth.Token(key=Access_Token,secret=Access_Token_Secret)
+client = oauth.Client(consumer,access_token)
+
+
+
+import urllib
+url = "https://api.twitter.com/1.1/statuses/update.json"
+mybody=urllib.urlencode({'status': 'Hello World'})
+response,content=client.request(url,method='POST',body=mybody)
+
+
+
+import io
+with io.open('src/ds_twitter_1.json', 'w', encoding='utf8') as json_file:
+    data=json.dumps(content, json_file, ensure_ascii=False, encoding='utf8')
+    json_file.write(data)
