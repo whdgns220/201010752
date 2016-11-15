2e70e2becab902d46e621029b29d8ec19719f5b2
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-11-15 10:13:20 +0900

    Add files via upload

---
+++ Twitter T-3(seoul 10).py
@@ -0,0 +1,54 @@
+﻿T-3: Twitter에 'Seoul'을 10개 검색한다.
+
+
+OAuth
+
+
+import json
+import oauth2 as oauth
+
+Consumer_Key = "dc0eZ3zFaOFFdkUoWmIVms72W"
+Consumer_Secret = "qOQMweqA3GT0XqAyK9bWVTXS9TZJfHj78fBRvBVBzxqJRFmrNy"
+Access_Token = "786057611166773249-WYglj3fxUPW23sOfBRJboVAiIF5gaAt"
+Access_Token_Secret = "AZNzz8C2edrA85L0Mvg6WLKC0RUO6onKVyiVkcNe7BKti"
+
+consumer = oauth.Consumer(key=Consumer_Key, secret=Consumer_Secret)
+access_token = oauth.Token(key=Access_Token,secret=Access_Token_Secret)
+client = oauth.Client(consumer,access_token)
+
+
+
+
+
+
+import urllib
+url1 = "https://api.twitter.com/1.1/search/tweets.json"
+myparam={'q':'seoul','count':20}
+mybody=urllib.urlencode(myparam)
+
+resp, tsearch = client.request(url1+"?"+mybody, method="GET")
+tsearch_json = json.loads(tsearch)
+
+
+
+
+
+
+print type(tsearch_json)
+print tsearch_json.keys()
+print len(tsearch_json['statuses'])
+
+
+
+
+len(tsearch_json['statuses'][0])
+
+
+
+
+for i,tweet in enumerate(tsearch_json['statuses']):
+    #print tweet[u'user'][u'name']
+    print "[%d]\t%d\t%s:%s" % (i,tweet['id'],tweet['user']['name'],tweet['text'])
+
+
+
