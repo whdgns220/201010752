01bbf68918bd33a74c817bbeec61eebe52178d90
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-11-23 17:51:07 +0900

    Add files via upload

---
+++ S-2 Hello Spark.py
@@ -0,0 +1,34 @@
+﻿## 2. Hello Spark
+
+
+## 처음에 프롬프트에서 pip install pyspark 를 설치한 이후에 밑에 실행할 것!!
+
+import findspark
+
+import os
+os.getcwd()
+
+import os
+spark_home="C:/Users/DB400T2A/Downloads/spark-1.6.0-bin-hadoop2.6"
+
+print spark_home
+
+findspark.init(spark_home)
+
+import pyspark
+
+conf=pyspark.SparkConf()
+
+conf=pyspark.SparkConf().setAppName("myApp")
+
+sc=pyspark.SparkContext(conf=conf)
+
+sc
+
+sc.version
+
+sc.master
+
+sc._conf.getAll()
+
+
