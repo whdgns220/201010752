13323bed053258bd288b3be8e315714cd01a0bf7
201010752 임종훈(과제파일 : 201010752) <whdgns220@nate.com>
2016-12-05 09:37:00 +0900

    Update S-3 Hello RDD.py

--- S-3 Hello RDD.py
+++ S-3 Hello RDD.py
@@ -1,4 +1,35 @@
-﻿# 문제 S-3: Hello RDD
+import findspark
+
+import os
+os.getcwd()
+
+import os
+spark_home="C:/Users/DB400T2A/Downloads/spark-1.6.0-bin-hadoop2.6"
+
+print spark_home
+
+findspark.init(spark_home)
+
+import pyspark
+
+conf=pyspark.SparkConf()
+
+conf=pyspark.SparkConf().setAppName("myApp")
+
+sc=pyspark.SparkContext(conf=conf)
+
+sc
+
+sc.version
+
+sc.master
+
+sc._conf.getAll()
+
+
+
+
+# 문제 S-3: Hello RDD
 
 
 c=list([36.5,32,35,37.3,30])
